30d7570ca4f441ec7a24be09486cc610537ad223
László Dorogi <laszlo.dorogi.68@gmail.com>
2018-08-26 08:32:16 +0200

    Submission error correction

--- src/Keys.js
+++ src/Keys.js
@@ -1,5 +1,6 @@
 // Google Maps API key
 export const GOOGLE_MAP_KEY = 'AIzaSyAbHovFzwvdwf81BCJo0HF3YMN6lfwqsVY';
+
 // Foursquare Client ID
 export const FOURSQUARE_CLI_ID = 'GFM3BOP5JNJRHBHVKDG1HZLP33M2VKFIZIRCVOTEDLONKXRX';
 // Foursquare Client Secret
